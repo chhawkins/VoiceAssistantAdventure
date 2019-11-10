2ec8b00d037de829360787cebbe7d689a0e69805
chhawkins <chhawkins@ksu.edu>
2019-11-10 08:55:15 -0600

    more game functionality

--- VAA_Site/game/game_state_machine.py
+++ VAA_Site/game/game_state_machine.py
@@ -1,3 +1,5 @@
+import pickle
+
 states = {
     'a':
         {'look': ['bookcase', 'floor', ''],
@@ -32,18 +34,45 @@ move = {
         {
             'left': 'c',
             'right': 'd'
+        },
+    'c':
+        {
+            'forward': 'e'
+        },
+    'd':
+        {
+            'forward': 'e'
         }
 }
 
+activate = {
+    'a':
+        {
+            'switch': 'forward',
+            'book': 'forward',
+            'bookcase': 'forward'
+        },
+    'c':
+        {'': 'forward'},
+    'd':
+        {'': 'forward'}
+}
+''' Initial state
 path_open = {
     'a': {
         'b': False
     },
     'b': {
-        'c': False,
-        'd': False
+        'c': True,
+        'd': True
+    },
+    'c': {
+        'e': False
+    },
+    'd': {
+        'e': False
     }
-}
+}'''
 
 
 def action_legal(state, action, target):
@@ -54,6 +83,9 @@ def perform_action(action, action_modifier, target, parameter):
     file = open('VAA_Site/game/state.txt', 'r')
     state = file.read()
     file.close()
+    with open(r'path_open.pickle', 'rb') as input_file:
+        path_open = pickle.load(input_file)
+
     if action_legal(state, action, target):
         if action == 'cast':
             return cast_text(action_modifier, target)
@@ -61,10 +93,16 @@ def perform_action(action, action_modifier, target, parameter):
             return look_text(state, target)
         if action == 'move':
             if path_open.get(move.get(state).get(action_modifier)):
+                oldState = state
                 state = move.get(state).get(action_modifier)
                 file = open('VAA_Site/game/state.txt', 'w')
                 file.write(state)
                 file.close()
+                return move_text(oldState, state)
+        if action == 'activate':
+            path_open.get(state).set(move.get(state).get(activate.get(state).get(target)), True)
+            with open(r'VAA_Site/game/path_open.pickle', 'wb') as out_file:  # writes pickle file
+                pickle.dump(path_open, out_file)
 
 
 def cast_text(action_modifier, target):
@@ -76,3 +114,10 @@ def look_text(state, target):
         return 'You look at the', target, 'and see', look.get(state).get(target)
     else:
         return 'You look around and see', look.get(state).get('')
+
+
+def move_text(old_state, state):
+    return "You move from room", old_state, 'to room', state
+
+# with open(r'./path_open.pickle', 'wb') as out_file:  # writes pickle file
+#     pickle.dump(path_open, out_file)
