7835548536465e23d1fb0f886881a9cb4f3756a9
chhawkins <chhawkins@ksu.edu>
2019-11-10 08:15:42 -0600

    more game functionality

--- VAA_Site/game/game_state_machine.py
+++ VAA_Site/game/game_state_machine.py
@@ -7,15 +7,42 @@ states = {
     'b':
         {'listen': ['left', 'right'],
          'move': ['left', 'right'],
-         }
+         'look': ['around', '']}
 }
 
 look = {
     'a':
         {
-            'around': "its about as average as a bar can get, people milling about, lurkers around the outskirts of the room, shady figures in the shadows",
+            '': "its about as average as a bar can get, people milling about, lurkers around the outskirts of the room, shady figures in the shadows",
             'floor': "you see scratches on the floor near the bookcase, maybe signs that it has moved recently",
-            'bookcase': "the bookcase is fairly new and sturdy, but it looks highly worn in a spot near the book 'Python: For Dummies'"}
+            'bookcase': "the bookcase is fairly new and sturdy, but it looks highly worn in a spot near the book 'Python: For Dummies'"},
+    'b':
+        {
+            '': "you are in a room that branches to 2 areas, with a path going left and a path going right"
+        }}
+
+move = {
+    'a':
+        {
+            'forward': 'b',
+            'tunnel': 'b',
+            'in': 'b'
+        },
+    'b':
+        {
+            'left': 'c',
+            'right': 'd'
+        }
+}
+
+path_open = {
+    'a': {
+        'b': False
+    },
+    'b': {
+        'c': False,
+        'd': False
+    }
 }
 
 
@@ -24,12 +51,20 @@ def action_legal(state, action, target):
 
 
 def perform_action(action, action_modifier, target, parameter):
-    state = open('VAA_Site/game/state.txt', 'rw').read()
+    file = open('VAA_Site/game/state.txt', 'r')
+    state = file.read()
+    file.close()
     if action_legal(state, action, target):
         if action == 'cast':
             return cast_text(action_modifier, target)
         if action == 'look':
             return look_text(state, target)
+        if action == 'move':
+            if path_open.get(move.get(state).get(action_modifier)):
+                state = move.get(state).get(action_modifier)
+                file = open('VAA_Site/game/state.txt', 'w')
+                file.write(state)
+                file.close()
 
 
 def cast_text(action_modifier, target):
@@ -37,7 +72,7 @@ def cast_text(action_modifier, target):
 
 
 def look_text(state, target):
-    if target != '':
+    if target != '' and target != 'around':
         return 'You look at the', target, 'and see', look.get(state).get(target)
     else:
-        return 'You look around and see', look.get(state).get(target)
+        return 'You look around and see', look.get(state).get('')
